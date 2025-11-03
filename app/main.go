e0740b1b9948a5e14f3219a4203c4e28ed471ead
Salimzyanov Lenar <salimzyanov_lenar@mail.ru>
2025-11-03 19:25:52 +0400

    added exit handler

--- app/main.go
+++ app/main.go
@@ -11,11 +11,38 @@ import (
 var _ = fmt.Fprint
 var _ = os.Stdout
 
+func handle_exit(command string) bool {
+	splited := strings.Fields(command)
+
+	if len(splited) != 2 {
+		return false
+	}
+
+	cmd := splited[0]
+	status_code := splited[1]
+
+	if cmd != "exit" {
+		return false
+	}
+
+	if status_code == "0" {
+		return true
+	}
+
+	return false
+
+}
+
 func main() {
 	// REPL
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if handle_exit(command) {
+			break
+		}
+
 		command = strings.TrimSpace(command)
 		fmt.Printf("%s: command not found\n", command)
 	}
