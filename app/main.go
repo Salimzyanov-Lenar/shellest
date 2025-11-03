bfdd8e6da7ddf6762f5ee90b383fdd75ff3ed937
Salimzyanov Lenar <salimzyanov_lenar@mail.ru>
2025-11-03 21:49:43 +0400

    z

--- app/main.go
+++ app/main.go
@@ -4,86 +4,85 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"path/filepath"
+	"slices"
+	"strconv"
 	"strings"
 )
 
-var commandHandlers map[string]func([]string)
-
-func init() {
-	commandHandlers = map[string]func([]string){
-		"echo": echo,
-		"exit": exit,
-		"type": checkType,
-	}
-}
-
-func exit(commands []string) {
-	// Exit handler
-	if len(commands) == 1 {
-		os.Exit(0)
-	}
-	if len(commands) > 2 {
-		fmt.Fprintln(os.Stdout, "exit: too many arguments")
-		return
-	}
-	if len(commands) == 2 && commands[1] == "0\n" {
-		os.Exit(0)
-	}
-}
-
-func echo(commands []string) {
-	// Echo handler
-	if len(commands) >= 1 {
-		fmt.Fprint(os.Stdout, strings.Join(commands[1:], " "))
-		return
-	}
-}
-
-func checkPathInSystem(commands []string) bool {
-	// Type handler for check program in system
-	paths := os.Getenv("PATH")
-
-	command := strings.TrimSpace(commands[1])
-
-	for _, path := range strings.Split(paths, ":") {
-		file := filepath.Join(path, command)
-		if _, err := os.Stat(file); err == nil {
-			fmt.Fprintf(os.Stdout, "%s is %s\n", command, file)
-			return true
-		}
-	}
-	return false
-}
-
-func checkType(commands []string) {
-	// Type handler
-	command := strings.TrimSpace(commands[1])
-	_, ok := commandHandlers[command]
-	if ok {
-		fmt.Fprintln(os.Stdout, fmt.Sprint(command, " is a shell builtin"))
-		return
-	}
-	if !checkPathInSystem(commands) {
-		fmt.Fprintln(os.Stdout, strings.TrimRight(strings.Join(commands[1:], ""), "\n")+": not found")
-	}
-}
+var builtIns = []string{"type", "echo", "exit"}
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
+
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
 		}
 
-		commands := strings.Split(input, " ")
-		handler, ok := commandHandlers[commands[0]]
-		if !ok {
-			fmt.Fprintln(os.Stdout, input[:len(input)-1]+": command not found")
-			continue
+		argv := strings.Fields(input)
+		cmd := argv[0]
+
+		switch cmd {
+		case "exit":
+			ExitCommand(argv)
+		case "echo":
+			EchoCommand(argv)
+		case "type":
+			TypeCommand(argv)
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
 		}
-		handler(commands)
 	}
+}
+
+func ExitCommand(argv []string) {
+	code := 0
+
+	if len(argv) > 1 {
+		argCode, err := strconv.Atoi(argv[1])
+		if err != nil {
+			code = argCode
+		}
+	}
+
+	os.Exit(code)
+}
+
+func EchoCommand(argv []string) {
+	output := strings.Join(argv[1:], " ")
+	fmt.Fprintf(os.Stdout, "%s\n", output)
+}
+
+func TypeCommand(argv []string) {
+	if len(argv) == 1 {
+		return
+	}
+
+	value := argv[1]
+
+	if slices.Contains(builtIns, value) {
+		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", value)
+		return
+	}
+
+	if file, exists := findBinInPath(value); exists {
+		fmt.Fprintf(os.Stdout, "%s is %s\n", value, file)
+		return
+	}
+
+	fmt.Fprintf(os.Stdout, "%s: not found\n", value)
+}
+
+func findBinInPath(bin string) (string, bool) {
+	paths := os.Getenv("PATH")
+	for _, path := range strings.Split(paths, ":") {
+		file := path + "/" + bin
+		if _, err := os.Stat(file); err == nil {
+			return file, true
+		}
+	}
+
+	return "", false
 }
